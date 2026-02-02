181a716b92d8cadbc9a118a7427d832d1e496d70
eno <eno@open-lab.org>
2026-02-02 11:16:35 +0100

    fix

--- simple-go/json/json.go
+++ simple-go/json/json.go
@@ -6,20 +6,18 @@ import (
 )
 
 func ToJson(v interface{}) string {
-	// Format as JSON array
 	result, err := json.Marshal(v)
 	if err != nil {
-		panic(fmt.Sprintf("Error encoding result: %v", err))
+		panic(fmt.Sprintf("Error JSON-encoding: %v", err))
 	}
 
 	return string(result)
 }
 
 func ToPrettyJson(v interface{}) string {
-	// Format as JSON array
 	result, err := json.MarshalIndent(v, "", "  ")
 	if err != nil {
-		panic(fmt.Sprintf("Error encoding result: %v", err))
+		panic(fmt.Sprintf("Error JSON-encoding: %v", err))
 	}
 
 	return string(result)
